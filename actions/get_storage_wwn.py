254cc3b8eaa45725d1ac7391679548cb698c8bb3
Aurimas Žagunis <aurimas.zagunis@virtustream.com>
2019-08-28 14:18:14 +0300

    Update get_storage_wwn.py

--- actions/get_storage_wwn.py
+++ actions/get_storage_wwn.py
@@ -1,21 +1,3 @@
-# Licensed to the StackStorm, Inc ('StackStorm') under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import eventlet
-import json
-
 from pyVmomi import vim  # pylint: disable-msg=E0611
 
 from vmwarelib import inventory
@@ -24,7 +6,6 @@ from vmwarelib.actions import BaseAction
 
 
 class GetStorageWwn(BaseAction):
-
     def run(self, storage_name, vm_name, vsphere=None):
         """
         Retrieve Storage WWN
@@ -53,6 +34,5 @@ class GetStorageWwn(BaseAction):
                 for te in d.info.vmfs.extent:
                     result = {'name': d.name, 'wwn': te.diskName}
             else:
-                return
-
+                return result
         return result
