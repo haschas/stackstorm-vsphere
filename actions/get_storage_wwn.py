a752d78038630f5f8d74c84f709e1062912bdd07
Aurimas Žagunis <aurimas.zagunis@virtustream.com>
2019-08-30 14:31:12 +0300

    get_storage_wwn action support multiple storages
    get_storage_wwn now retrieves WWNs for multiple storages.

--- actions/get_storage_wwn.py
+++ actions/get_storage_wwn.py
@@ -6,38 +6,34 @@ from vmwarelib.actions import BaseAction
 
 
 class GetStorageWwn(BaseAction):
-    def run(self, storage_name, vm_name, vsphere=None):
+    def run(self, storage_names, vsphere=None):
         """
         Retrieve Storage WWN
 
         Args:
-        - storage_name: Storage name
-        - vm_name: Name for VM to lookup for storages and their WWNs
+        - storage_names: Storage name
         - vsphere: Pre-configured vsphere connection details (config.yaml)
 
         Returns:
         - dict: Success
         """
-        # ensure that minimal inputs are provided
-        checkinputs.one_of_two_strings(storage_name, vm_name, "Storage or VM Name")
-
         self.establish_connection(vsphere)
         result, error = None, None
 
-        # getting information for provided storage
-        #datastore_container = inventory.get_resources(self.si_content, vim.Datastore)
+        # getting information for provided storages
         datastore_container = self.si_content.viewManager.CreateContainerView(self.si_content.rootFolder, [vim.Datastore], True)
         datastores_list = datastore_container.view
         datastore_container.Destroy()
 
-        # get datastore object
-        ds_obj = inventory.get_datastore(self.si_content, name=storage_name)
+        # list of datastores wwns
+        ds_wwns = []
 
-        if ds_obj in datastores_list:
-            # iterate through datastores
-            for d in datastores_list:
-                if d.name == storage_name:
-                    for te in d.info.vmfs.extent:
-                        return ({'name': d.name, 'wwn': te.diskName})
-        else:
-            return (False, {'state': False, 'msg': 'Datastore not found'})
+        # iterate through datastores
+        for d in datastores_list:
+            if d.name in storage_names:
+                for te in d.info.vmfs.extent:
+                    ds_wwns.append({'datastore': d.name, 'wwn': te.diskName})
+
+        result = ds_wwns
+
+        return result
