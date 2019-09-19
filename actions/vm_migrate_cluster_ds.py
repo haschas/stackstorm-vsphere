0d3d861f8b5130860e2da72d8c9fddcc42daf006
Aurimas Žagunis <aurimas.zagunis@virtustream.com>
2019-09-19 10:09:22 +0300

    vm_migrate_cluster_ds action udate
    Fixed VM migration to proper storage vmotion.

--- actions/vm_migrate_cluster_ds.py
+++ actions/vm_migrate_cluster_ds.py
@@ -1,5 +1,6 @@
 from pyVmomi import vim  # pylint: disable-msg=E0611
 import sys
+import random
 
 from vmwarelib import inventory
 from vmwarelib import checkinputs
@@ -27,17 +28,34 @@ class MigrateVmClDs(BaseAction):
         self.establish_connection(vsphere)
 
         vm = inventory.get_virtualmachine(self.si_content, vm_id, vm_name)
-        spec = vim.vm.ConfigSpec()
         # Getting RID code from VM name
         rid = vm_name[:4]
         ds_size_list = []
-        disk_size = 0
+        rp_usage_list = []
+        dest_hosts = []
+        foh_hosts = []
+
         cluster_container = inventory.get_managed_entities(self.si_content, vim.ClusterComputeResource)
         cluster_list = cluster_container.view
         cluster_container.Destroy()
+
         # iterate through clusters
         for cl in cluster_list:
             if cl.name == cluster:
+                # Resource Pool
+                # Checking if there is any failover hosts in the cluster
+                # adding those to separate list, further it will be comapred
+                # with hosts that are attached to matched datastore 
+                if hasattr(cl.configuration.dasConfig.admissionControlPolicy, 'failoverHosts'):
+                    for foh in cl.configuration.dasConfig.admissionControlPolicy.failoverHosts:
+                        foh_hosts.append(foh)
+                for rp in cl.resourcePool.resourcePool:
+                    # Generating list of all available resource pools
+                    # matching RID
+                    if rp.name[:4] == rid:
+                        overallUsage = rp.summary.runtime.memory.overallUsage + rp.summary.runtime.cpu.overallUsage
+                        rp_usage_list.append((rp, overallUsage))
+                # Datastore
                 datastores = cl.datastore
                 # iterate through datastores that
                 # exists under specified cluster
@@ -45,19 +63,49 @@ class MigrateVmClDs(BaseAction):
                     ds = datastore.summary
                     # take datastores that has proper RID
                     if ds.name[:4] == rid:
+                        # getting hosts that are attached to datastore
+                        # further those will be compared with FOH hosts
+                        for h in datastore.host:
+                            dest_hosts.append(h.key)
                         ds_size_list.append((ds.datastore, ds.freeSpace))
+        
+        # If list is empty, no storages found
+        if len(ds_size_list) == 0:
+            return (False, {'state': False, 'msg': 'No storages found in cluster {} for rid {}.'.format(cluster, rid)})
+
         ds_to_migrate_to = max(ds_size_list, key = lambda i : i[1])[0]
+
+        # Checking target resource pool list
+        if len(rp_usage_list) == 0:
+            return (False, {'state': False, 'msg': 'No resource pools found in cluster {} for rid {}.'.format(cluster, rid)})
+
+        rp_to_mirgate_to = min(rp_usage_list, key = lambda i : i[1])[0]
+
+        # Determing to which host VM should be migrated to
+        # on destination cluster
+        ok_hosts = list(set(dest_hosts)^set(foh_hosts))
+        dest_host = random.choice(ok_hosts)
+        
+        # Migrate VM only if datastores not matching destination one
         if ds_to_migrate_to not in vm.datastore:
-            # relocate spec, to migrate vm to another datastore
-            # this can be updated with other migrations like:
-            # resource pool, host
-            relocate_spec = vim.vm.RelocateSpec(datastore=ds_to_migrate_to)
-            # does the actual migration
-            relocate_vm = vm.Relocate(relocate_spec)
+            # Setting vm relocation specification
+            vm_relocate_spec = vim.vm.RelocateSpec()
+            vm_relocate_spec.host = dest_host
+            vm_relocate_spec.pool = rp_to_mirgate_to
+            vm_relocate_spec.datastore = ds_to_migrate_to
+            # Performing actual migration
+            relocate_vm = vm.Relocate(spec=vm_relocate_spec)
+        # If VM is already on the same storage
         else:
             return (False, {'state': False, 'msg': 'VM is on proper datastore, will not migrate.'})
+
         successfully_relocated_vm = self._wait_for_task(relocate_vm)
         if successfully_relocated_vm != True:
             return (False, {'state': successfully_relocated_vm, 'msg': relocate_vm})
         else:
-            return {'state': successfully_relocated_vm}
+            return {
+                    'state': successfully_relocated_vm, 
+                    'dest_datastore': ds_to_migrate_to.name, 
+                    'dest_cluster': cluster, 
+                    'dest_resource_pool': rp_to_mirgate_to.name
+                    }
